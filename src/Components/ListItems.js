eb02e04a0622fc6f232ea59256c41bd92248f27e
DESKTOP-19AN2AD\SonataDev <edgar.hernandez@sonatasmx.com>
2019-07-30 22:03:49 -0700

    Using hooks

--- src/Components/ListItems.js
+++ src/Components/ListItems.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonsItems from './ButtonsItems.js';
 import { connect } from 'react-redux'
 import changeDetail from '../Actions/ChangeDetail.js'
 
+
 function ListItems (props) {
+    const [title, setTitle] = useState('List Items: ');
     var itemsToRender = props.foundItems.map((cat) => 
         <div className="row"> 
             <ButtonsItems textDisplay={cat.name || cat.title} widthKey={1} colorKey={"gray"} operation={() => props.changeDetail(JSON.stringify(cat))}/>
@@ -11,11 +13,18 @@ function ListItems (props) {
     );
     return (
     <div className="ListItems">
-        {itemsToRender}
+        <div>
+            {title}
+        </div>
+        <div>
+            {itemsToRender}
+        </div>
     </div>
     );
   }
-  
+
+
+
  const mapStateToProps = (state) => { 
     //console.dir(state);
     return {foundItems: state.starWars.foundItems}};

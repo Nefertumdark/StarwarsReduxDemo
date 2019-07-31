f5f0e341cd04509f84f5ec8933e66848a6cbfb46
DESKTOP-19AN2AD\SonataDev <edgar.hernandez@sonatasmx.com>
2019-07-30 21:18:23 -0700

    Using redux-forms

--- src/Components/SearchSection.js
+++ src/Components/SearchSection.js
@@ -2,19 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux'
 import updateResults from '../Actions/UpdateResults.js'
 import ListItems from './ListItems.js';
+import { Field, reduxForm } from 'redux-form';
 
-function SearchSection (props) {
-    return (
-    <div>
-        <div className="SearchSection">
-            <input type="text" value={props.filter} onChange={(e) => props.updateResults(props.searchKey, e.target.value)}/>
-        </div>
-        <div className="col-sm-4"><ListItems/></div>
-    </div> 
-    );
-  }
+class SearchSection extends React.Component {
+    renderInput(formProps){
+        return <input {...formProps.input} />;
+    }
+
+    onSubmit = (formProps) => {
+        this.props.updateResults(this.props.searchKey, formProps.searchFilter);
+    }
+    
+    render(){
+        return (
+            <div>
+                <div className="SearchSection">
+                    <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                        <Field name="searchFilter" component={this.renderInput} />
+                    </form>
+                </div>
+                <div className="col-sm-4"><ListItems/></div>
+            </div> 
+        );
+    }
+}
 
 const mapStateToProps = (state) => { 
     console.dir(state);
     return {searchKey: state.starWars.searchKey, filter: state.starWars.filter}};
-export default connect(mapStateToProps, {updateResults})(SearchSection);	
+export default reduxForm({
+    form: 'streamCreate'
+})(connect(mapStateToProps, {updateResults})(SearchSection));
--- src/ConfigureStore.js
+++ src/ConfigureStore.js
@@ -1,9 +1,10 @@
 import starWarsReducer from './Reducers/StarWarsReducer.js'			
 
 import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { reducer } from 'redux-form'
 import thunk from 'redux-thunk'
 
-const rootReducer = combineReducers({starWars: starWarsReducer});
+const rootReducer = combineReducers({starWars: starWarsReducer, form: reducer});
 
 export default function configureStore(initialState) {		
 	return createStore(
